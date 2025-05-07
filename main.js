17fde287cc46b66a2ad974630b08b516ecc5236c
Prince Asante <iamprinceasante@gmail.com>
2025-05-07 15:15:02 +0000

    Done with logics

--- main.js
+++ main.js
@@ -60,14 +60,24 @@ const start_game = (user_choice) => {
   // Let computer make a choice
   const computer_choice = computer_options[Math.floor(Math.random() * 3)];
 
-  // Compare choices
+  // Compare choices and predict the winner
   const choices = [user_choice, computer_choice];
   const results = possible_outcomes.filter((outcome) => {
     return JSON.stringify(outcome.option) === JSON.stringify(choices);
   });
 
-  console.log(results);
-
-  // Predict winner
   // Show results
+  document.querySelector(".results-container").innerHTML = results[0].message;
+
+  // Show choices
+  document.querySelector("#human").innerHTML = user_choice;
+  document.querySelector("#computer").innerHTML = computer_choice;
+  document.querySelector(".choice").style.display = "block";
+
+  // Show reset button
+  document.querySelector("#reset").style.display = "block";
+};
+
+const reset_game = () => {
+  window.location.reload();
 };
