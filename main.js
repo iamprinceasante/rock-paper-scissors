a4ff90af7c7e88b5265aa24e10ecfdf359b3d039
Prince Asante <iamprinceasante@gmail.com>
2025-05-09 16:19:50 +0000

    Optimization of js

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
-// Get human choice
+// Select DOM Element
 const human_choices = document.querySelectorAll(".choices");
-const images = document.querySelectorAll(".image");
+const all_images = document.querySelectorAll(".image");
 const reset_button = document.querySelector("#reset");
 const results_container = document.querySelector(".results-container");
 
-// Handle Human Choice
-const userClick = (e) => {
-  e.addEventListener("click", () => {
-    start_game(e.id);
-    human_choices.forEach((choice) => {
-      choice.style.pointerEvents = "none";
-    });
-  });
-};
-
-human_choices.forEach((e) => {
-  userClick(e);
-});
-
-// Get computer choice
+// Computer choice
 const computer_options = ["rock", "paper", "scissors"];
 
 // Possible outcomes
@@ -62,18 +48,34 @@ const possible_outcomes = [
   },
 ];
 
+// User click function
+const userClick = (e) => {
+  e.addEventListener("click", () => {
+    start_game(e.id);
+    human_choices.forEach((choice) => {
+      choice.style.pointerEvents = "none";
+    });
+  });
+};
+
+// Attach event listener to each choice button
+human_choices.forEach((e) => {
+  userClick(e);
+});
+
+// Game Start
 const start_game = (user_choice) => {
   // Let computer make a choice
   const computer_choice = computer_options[Math.floor(Math.random() * 3)];
 
-  // Compare choices and predict the winner
+  // Determine outcome based on human and computer choice
   const choices = [user_choice, computer_choice];
   const results = possible_outcomes.filter((outcome) => {
     return JSON.stringify(outcome.option) === JSON.stringify(choices);
   });
 
-  // Show results
-  document.querySelector(".results-container").innerHTML = results[0].message;
+  // Display result message and color
+  results_container.innerHTML = results[0].message;
   if (results[0].message === "Draw") {
     document.querySelector(".results-container").style.color = "#d7eb23";
   } else if (results[0].message === "You win") {
@@ -82,8 +84,8 @@ const start_game = (user_choice) => {
     document.querySelector(".results-container").style.color = "#f02c2c";
   }
 
-  // Show choices
-  images.forEach((e) => {
+  // Update image styles to reflect choices
+  all_images.forEach((e) => {
     e.classList.remove("shadow");
   });
   if (results[0].message !== "Draw") {
@@ -93,23 +95,18 @@ const start_game = (user_choice) => {
     document.querySelector(`.${user_choice}`).classList.add("draw");
   }
 
-  // Scores
-
   // Show reset button
   reset_button.classList.replace("hide", "display");
 };
 
 const reset_game = () => {
-  console.log(images);
-
   // Reset visuals
-  images.forEach((e) => {
+  all_images.forEach((e) => {
     e.classList.remove("human");
     e.classList.remove("computer");
     e.classList.remove("draw");
     e.classList.add("shadow");
   });
-  console.log(images);
 
   // Reset result text
   results_container.innerHTML = "";
